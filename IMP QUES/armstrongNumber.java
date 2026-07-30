2ec65a1fbe78dd2ee286ad31b351705e38c8f4e2
kartiksingh3129-bit <kartikmahara500@gmail.com>
2026-07-30 12:28:47 +0530

    .

---
+++ IMP QUES/armstrongNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class armstrongNumber {
+    public armstrongNumber() {
+    }
+
+    public static void main(String[] args) {
+        int arm = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER ANY NUMBER : ");
+        int num = scanner.nextInt();
+
+        int a;
+        int d;
+        for(d = num; num > 0; arm += a * a * a) {
+            a = num % 10;
+            num /= 10;
+        }
+
+        if (arm == d) {
+            System.out.println("ARMSTRONG NUMBER ");
+        } else {
+            System.out.println("NOT A ARMSTRONG NUMBER ");
+        }
+
+    }
+}
