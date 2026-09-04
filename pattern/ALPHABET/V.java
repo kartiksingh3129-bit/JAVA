daf4e03a61591e09208dd282cd164cee86be82bb
kartiksingh3129-bit <kartikmahara500@gmail.com>
2026-09-04 09:45:37 +0530

    .

--- pattern/ALPHABET/V.java
+++ pattern/ALPHABET/V.java
@@ -1,3 +1,16 @@
 public class V {
-    
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 13; j++) {
+
+                if (j == i || j == 14 - i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
+            System.out.println();
+        }
+    }
 }
